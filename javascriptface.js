5f9545714ae85e848cfb547398d5cf35a1e93c34
joaquin daurat <joako.daurat@gmail.com>
2019-09-12 15:00:37 -0300

    asd

--- javascriptface.js
+++ javascriptface.js
@@ -66,7 +66,7 @@ function shareOriginal()
 {
   $('meta[property="og:title"]').replaceWith('<meta property="og:title" content="New Title">');
 
-FB.api('post', { id: 'https://joakodaurat.github.io/sharingfacebook/',scrape:true }, function(response) {
+FB.api('363398301275047', 'post', {scrape:true }, function(response) {
   if (!response || response.error) {
     alert('Error occured');
   } else {
